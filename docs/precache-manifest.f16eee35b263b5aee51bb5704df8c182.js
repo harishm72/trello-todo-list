ca53db736414511d342dd4aac589b59251fc8691
harishm72 <harishchowdary72@gmail.com>
2022-09-17 22:28:25 +0530

    fix: push

---
+++ docs/precache-manifest.f16eee35b263b5aee51bb5704df8c182.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d0be71772d3d9b61d82",
+    "url": "/trello-todo-list/static/css/main.aa7ce917.chunk.css"
+  },
+  {
+    "revision": "1d0be71772d3d9b61d82",
+    "url": "/trello-todo-list/static/js/main.1d0be717.chunk.js"
+  },
+  {
+    "revision": "999a0f3b9d5d552b3cc5",
+    "url": "/trello-todo-list/static/js/1.999a0f3b.chunk.js"
+  },
+  {
+    "revision": "f2b69e919c388231ba74",
+    "url": "/trello-todo-list/static/js/runtime~main.f2b69e91.js"
+  },
+  {
+    "revision": "fa8174ecb617bd8ed8a7fe05f27c0a78",
+    "url": "/trello-todo-list/index.html"
+  }
+];
